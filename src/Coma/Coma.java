4c7c23a0d0908ead0da1428dbe335570dc47e88c
Thanh <Thanh@LAPTOP-0NU4COTD.attlocal.net>
2017-05-09 20:52:38 -0700

    added basic foundation for switching between panels (still buggy though)

--- src/Coma/Coma.java
+++ src/Coma/Coma.java
@@ -17,13 +17,38 @@ public class Coma extends JFrame {
 		setBounds(100, 100, 800, 600);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    
-	    //gamePanel = new JPanel();
-	    OccipitalPanel panel = new OccipitalPanel();
+	    gamePanel = new JPanel();
+	    CardLayout c = new CardLayout();
+	    gamePanel.setLayout(c);
+	    
+	    HomeScreen panel1 = new HomeScreen(this);
+	    Menu panel2 = new Menu(this);
+	    Rules panel3 = new Rules(this);
+	    StoryScreen panel4 = new StoryScreen(this);
+	    
+	    gamePanel.add(panel1, "1");
+	    gamePanel.add(panel2,"2");
+	    gamePanel.add(panel3,"3");
+	    gamePanel.add(panel4, "4");
+	    
+	    add(gamePanel);
+	    addMouseListener(panel1);
+	    addMouseListener(panel2);
+	    addMouseListener(panel3);
+	    addMouseListener(panel4);
+//	    addKeyListener(panel4);
+	    
+//	    OccipitalPanel panel = new OccipitalPanel();
 	    //gamePanel.add(panel);
-	    panel.setVisible(true);
-	    add(panel);
+//	    panel.setVisible(true);
+//	    add(panel);
 	    
 	    setVisible(true);
+	}
+	
+	public void changePanel(String name) {
+		((CardLayout)gamePanel.getLayout()).show(gamePanel, name);
+		requestFocus();
 	}
 	
 	public static void main(String[] args) {
--- src/Coma/Menu.java
+++ src/Coma/Menu.java
@@ -1,7 +1,51 @@
 package Coma;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JPanel;
 
 
-public class Menu extends JPanel{
+public class Menu extends JPanel implements MouseListener{
 
+	private Coma c;
+	
+	public Menu(Coma c) {
+		this.c = c;
+		setBackground(Color.BLUE);
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
